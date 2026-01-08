166d2dc860c78ab9576229f64ab78b40441151cd
Git-Tutorial <mdsohail123g@gamil.com>
2026-01-08 21:13:16 +0530

    Dynamic signup - data Api

--- src/app.js
+++ src/app.js
@@ -3,18 +3,15 @@ const connectdb = require("./config/database");
 const User = require("./model/user");
 const app = express();
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
   // Create a new user instance
-  const userData = new User({
-    name: "Sohail",
-    lastName: "Ahmad",
-    email: "sohailAhmad@example.com",
-    password: "password123",
-    gender: "male",
-    age: 20,
-  });
+
+  const userData = new User(req.body);
   // Save the user to the database
-  try {              // Added try-catch for error handling
+  try {
+    // Added try-catch for error handling
     await userData.save();
     res.send("User signed up successfully");
     console.log("User data saved:");
