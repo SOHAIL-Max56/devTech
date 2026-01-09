7ad15a3dcb78630de9f0013756833fd323b1d6a1
Git-Tutorial <mdsohail123g@gamil.com>
2026-01-09 15:15:31 +0530

    GET API for users

--- src/app.js
+++ src/app.js
@@ -20,6 +20,30 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+app.get("/users", async (req, res) => {
+  const userEmail = req.body.email;
+  try {
+    // Finding the user which has same email
+    const user = await User.find({ email: userEmail });
+    if (user.length === 0) {
+      return res.status(404).send("User not found");
+    } else {
+      return res.status(200).send(user);
+    }
+  } catch (error) {
+    console.error("Error fetching user data:", error);
+  }
+});
+// To get all users data
+app.get("/feed", async (req, res) => {
+  const allusers = await User.find();
+  try {
+    res.status(200).send(allusers);
+  } catch (error) {
+    console.error("Error fetching all user data:", error);
+  }
+});
+// Connect to the database and start the servver
 connectdb()
   .then(() => {
     console.log("Database connected successfully...");
